72ced90f5c9138c17486611ddbc4cc14bbc6f247
Yohi <yahel.yedoav@gmail.com>
2025-11-29 09:16:51 +0200

    Added about page

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,23 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Hi, this is my about page, my name is Yohi and I am a software engineer.</p>
+        <p>I am a software engineer with a passion for building web applications and learning new technologies.</p>
+        <p>Using this website I&apos;m learning to:</p>
+        <ul>
+            <li>Use Next.js and Tailwind CSS.</li>
+            <li>Use React and Node.js.</li>
+            <li>Use MongoDB and MySQL.</li>
+            <li>Use Docker and Kubernetes.</li>
+            <li>Use AWS and Azure.</li>
+        </ul>
+        <p>And this website is to track the amount of books I read and the amount of books I want to read.</p>
+        <p>I hope you enjoy using this website and I hope you learn something new from it.</p>
+        <p>Thank you for visiting!</p>
+        <p>Sincerely,</p>
+        <p>Yohi</p>
+        <p>29/11/2025</p>
+    </div>
+  );
+}
